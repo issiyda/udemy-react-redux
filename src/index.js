9c02042f34724440554389c0f311c8989e0b9a3c
issiy <allmylifewriter@gmail.com>
2020-10-04 12:01:49 +0900

    event-delete イベント削除機能

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import {createStore, applyMiddleware } from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import './index.css';
 import reducer from './reducers'
-
 import EventsIndex from './components/events_index';
 import EventsNew from "./components/events_new";
+import EventsShow from "./components/events_show";
 import * as serviceWorker from './serviceWorker';
 
+const enhancer = process.env.NODE_ENV === 'development' ?
+    composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk);
 const store = createStore(reducer, applyMiddleware(thunk));
 
 
@@ -19,9 +22,11 @@ ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
             <Switch>
-                <Route exect path="/events/new" component={EventsNew}/>
+                <Route path="/events/new" component={EventsNew}/>
+                <Route path="/events/:id" component={EventsShow}/>
                 <Route exect path="/" component={EventsIndex}/>
-        <EventsIndex/>
+                <Route exect path="/events" component={EventsIndex}/>
+                <EventsIndex/>
             </Switch>
         </BrowserRouter>
     </Provider>,
